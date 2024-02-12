7ec247c5267987b83ef08824e78bc7f6ce90771a
murodilla <zaylobiddinovmurodilla1@gmail.com>
2024-02-12 23:36:06 +0500

    savollarni tuzishni boshladim

---
+++ js/index.js
@@ -0,0 +1,129 @@
+// alert("hello");
+
+alert(
+  "sizga bir nechta savollar beraman siz ularga ilji boricha tezroq va imlo xatolarsiz  javob bering"
+);
+
+var correct = 0;
+
+// var question1 = prompt(`1. Fransiyaning poytaxti qaysi shahar?
+
+// a) London
+// b) Parij
+// c) Berlin
+// d) Rim`);
+
+// console.log(question1);
+
+// if (question1 == "b" || question1 == "parij" || question1 == "Parij") {
+//   alert("good job");
+//   correct += 1;
+// } else {
+//   alert("xato qildizku");
+// }
+
+// var question2 = prompt(`2. Dunyoda nechta materik bor?
+
+// a) 6
+// b) 5
+// c) 7
+// d) 8`);
+
+// if (question2 == "a" || question2 == "6") {
+//   alert("good job");
+//   correct += 1;
+// } else {
+//   alert("xato qildizku");
+// }
+
+// var question3 = prompt(`3. “Romeo va Juletta”ni kim yozgan?
+
+// a) Uilyam Shekspir
+// b) Charlz Dikkens
+// c) Jeyn Osten
+// d) Mark Tven`);
+
+// if (
+//   question3 == "a" ||
+//   question3 == "Uilyam Shekspir" ||
+//   question3 == "uilyam shekspir"
+// ) {
+//   alert("good job");
+//   correct += 1;
+// } else {
+//   alert("xato qildizku");
+// }
+
+// var question4 = prompt(`4. Mendeleyev davriy jadvalida nechta element bor?
+
+// a) 128
+// b) 126
+// c) 63
+// d) 99`);
+
+// if (question4 == "c" || question4 == "63") {
+//   alert("good job");
+//   correct += 1;
+// } else {
+//   alert("xato qildizku");
+// }
+
+// var question5 = prompt(`5. Quyosh sistemamizdagi eng katta sayyora qaysi?
+
+// a) Yer
+// b) Mars
+// c) Yupiter
+// d) Venera`);
+
+// if (question5 == "c" || question5 == "Yupiter" || question5 == "yupiter") {
+//   alert("good job");
+//   correct += 1;
+// } else {
+//   alert("xato qildizku");
+// }
+
+// var question6 = prompt(`6.  9 ning kvadrat ildizi nechiga teng?
+
+// a) 2
+// b) 6
+// c) 4
+// d) 3`);
+
+// if (question6 == "d" || question6 == "3") {
+//   alert("good job");
+//   correct += 1;
+// } else {
+//   alert("xato qildizku");
+// }
+
+// var question7 = prompt(`7. Mona Liza rasmini kim chizgan?
+
+// a) Pablo Pikasso
+// b) Leonardo da Vinchi
+// c) Vinsent van Gog
+// d) Mikelanjelo`);
+
+// if (
+//   question7 == "b" ||
+//   question7 == "Leonardo da Vinchi" ||
+//   question1 == "leonardo da vinchi"
+// ) {
+//   alert("good job");
+//   correct += 1;
+// } else {
+//   alert("xato qildizku");
+// }
+
+// var question8 = prompt(`8. Braziliyada asosiy til qaysi?
+
+// a) ispan
+// b) ingliz
+// c) portugal
+// d) frantsuz`);
+
+// if (question8 == "c" || question8 == "portugal") {
+//   alert("good job");
+//   correct += 1;
+// } else {
+//   alert("xato qildizku");
+// }
